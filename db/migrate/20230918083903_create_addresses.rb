91f17d963bd9c3fa04f049962f475b99014db32f
EC2 Default User <ec2-user@ip-172-31-25-120.ap-northeast-1.compute.internal>
2023-09-19 05:04:04 +0000

    addresses,itemsカラムの追加

--- db/migrate/20230918083903_create_addresses.rb
+++ db/migrate/20230918083903_create_addresses.rb
@@ -3,6 +3,14 @@ class CreateAddresses < ActiveRecord::Migration[6.1]
     create_table :addresses do |t|
 
       t.timestamps
+      t.integer :customer_id,    null: false
+      t.string  :address,        null: false
+      t.string  :postal_code,    null: false
+      t.string  :name,           null: false
+      t.datetime:created_at,     null: false, default:now
+      t.datetime:updated_at,     null: false, default:now
+      
+      
     end
   end
 end
