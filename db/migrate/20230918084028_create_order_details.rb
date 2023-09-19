6f90f8c0620f2cd97d11e40a37e4f521cf340d57
miku0107 <138674231+miku0107@users.noreply.github.com>
2023-09-19 05:11:38 +0000

    マイグレートファイルadmin,order_detail

--- db/migrate/20230918084028_create_order_details.rb
+++ db/migrate/20230918084028_create_order_details.rb
@@ -1,6 +1,12 @@
 class CreateOrderDetails < ActiveRecord::Migration[6.1]
   def change
     create_table :order_details do |t|
+      
+      t.integer :order_id, null: false
+      t.integer :item_id, null: false
+      t.integer :amount, null: false
+      t.integer :unit_price, null: false
+      t.integer :make_status, null: false, default: 0
 
       t.timestamps
     end
