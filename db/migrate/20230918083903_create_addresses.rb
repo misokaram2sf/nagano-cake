e02ebf3823bf73be5c94fbec8bccc77e8756051e
EC2 Default User <ec2-user@ip-172-31-25-120.ap-northeast-1.compute.internal>
2023-09-19 05:14:27 +0000

    Add

--- db/migrate/20230918083903_create_addresses.rb
+++ db/migrate/20230918083903_create_addresses.rb
@@ -7,10 +7,9 @@ class CreateAddresses < ActiveRecord::Migration[6.1]
       t.string  :address,        null: false
       t.string  :postal_code,    null: false
       t.string  :name,           null: false
-      t.datetime:created_at,     null: false, default:now
-      t.datetime:updated_at,     null: false, default:now
+     
       
-      
+       t.timestamps
     end
   end
 end
