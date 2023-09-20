5ce128ecc7b7ec8a93ef9f3b045a525a13c58c2d
EC2 Default User <ec2-user@ip-172-31-25-120.ap-northeast-1.compute.internal>
2023-09-20 03:59:30 +0000

    会員登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  
+
+
   #顧客用
   devise_for :customers,controllers:{
     registrations: "public/registrations",
     sesisons: 'public/sessions'
-  }  
-  
+  }
+
   #管理者用
   devise_for :admin, controllers:{
     sessions: "admin/sessions"
   }
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
