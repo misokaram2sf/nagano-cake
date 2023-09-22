3b6838135613ec26e7ed6c1b76883cb29f157198
miku0107 <138674231+miku0107@users.noreply.github.com>
2023-09-22 05:53:42 +0000

    修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,23 +1,20 @@
 class Public::ItemsController < ApplicationController
 
-
+ 
   def index
     @items = Item.page(params[:page])
   end
-
+  
   def show
-
+   
     @cart_item_new = CartItem.new
-
+    
     @item = Item.find(params[:id])
-
   end
-
-
-
-
-
-
-
-
+  
+ 
+     
+  
+  
+    
 end
