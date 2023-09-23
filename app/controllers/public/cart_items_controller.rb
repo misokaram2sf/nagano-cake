fca482786703307bab29beba17afa23521d2d1a0
miku0107 <138674231+miku0107@users.noreply.github.com>
2023-09-23 14:38:26 +0000

    カート内アイテム　個別削除追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -43,9 +43,9 @@ class Public::CartItemsController < ApplicationController
     end
     
     def destroy
-        cart_item = CartItem.find(params[:id])
-        cart_item.destroy
-        redirect_to cart_item_path
+        @cart_item = CartItem.find(params[:id])
+        @cart_item.destroy
+        redirect_to cart_items_path
     end    
     
     #カート内アイテム全消去
