25857c44e76659f115188812ac1177b9acb9996e
EC2 Default User <ec2-user@ip-172-31-25-120.ap-northeast-1.compute.internal>
2023-09-23 02:45:50 +0000

    add 会員削除機能途中

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_18_084028) do
+ActiveRecord::Schema.define(version: 2023_09_22_133057) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
