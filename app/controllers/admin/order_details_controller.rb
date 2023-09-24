695ca69822f865264e989f2d8976f882b4b1a3c9
EC2 Default User <ec2-user@ip-172-31-43-89.ap-northeast-1.compute.internal>
2023-09-24 04:58:23 +0000

    order_detail途中

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,9 +1,18 @@
 class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def update
-      
+      @order_detail = OrderDetail.find(params[:order_detail_id])
+      @order = @order_detail.order
+      @order_details = @order.order_details
+      @order_detail.update(order_detail_params)
   end
-  
-  
+
+
+  private
+
+  def order_detail_params
+    params.require(:order_detail).permit(:make_status)
+  end
+
 end
