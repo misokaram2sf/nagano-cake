1a37424728bdb5a553030f62fb9959a89e601ea1
maki <141129686+kawasakimaki@users.noreply.github.com>
2023-09-25 19:31:17 +0000

    注文履歴詳細と注文履歴一覧

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,12 +1,29 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
 
+  def index
+    @orders = Order.all
+  end
+
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details
     @total_item_amount = @order_details.sum { |order_detail| order_detail.subtotal }
   end
 
+  def update
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: params[:id])
+    if @order.update(order_params)
+       @order_details.update_all(making_status: 1) if @order.status == "payment_confirmation"
+    end
+    redirect_to admin_order_path(@order)
+  end
 
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
 
 end
