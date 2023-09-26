157daf6f9e0afec4e52cb8bf717ac1c9a2f87bb1
maki <141129686+kawasakimaki@users.noreply.github.com>
2023-09-26 13:39:38 +0000

    ステータス関連

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,7 +2,7 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @orders = Order.page(params[:page])
+    @orders = Order.all
   end
 
   def show
