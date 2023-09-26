e288ba060822397c98ae4e5e713ec5e8ab0b8464
ansony062 <141472045+ansony062@users.noreply.github.com>
2023-09-26 09:30:37 +0000

    ジャンル検索

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -4,11 +4,13 @@ class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page]).per(12)
     @item_amount = Item.all
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem
+    @genres = Genre.all
   end
 
 
